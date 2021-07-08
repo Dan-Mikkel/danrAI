116dab93ad409947d03504a2509b54b7c53d2e30
Dan-Mikkel Hübenette <danmikkelhubenette@gmail.com>
2021-07-08 21:00:47 +0200

    Docs

--- src/processes/InitProcess.ts
+++ src/processes/InitProcess.ts
@@ -1,7 +1,11 @@
-import { Process } from "../Process";
 import { DanraiBundle, ProcessInfo } from "../index";
+import { Process } from "../Process";
 import { ProcessRegistry } from "../ProcessRegistry";
 
+/**
+ * The first process that gets launched.
+ * It will be in charge of initializing the other processes that this AI will use
+ */
 export class InitProcess extends Process {
   public constructor(pid: number, info: ProcessInfo) {
     super(pid, info);
